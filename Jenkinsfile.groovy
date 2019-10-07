f3416298ab1ab3f2b396753ffb6846b9becc2bf4
Safa Güner <safa.guner@getir.com>
2019-10-07 09:29:41 +0300

    ....edit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,16 @@ node {
     }
 
     stage('Select feature and tags') {
-        input id: 'User_input_id', message: 'Please select feature and tags', ok: 'Bir sonraki adıma geç', parameters: [[$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: '', filterLength: 1, filterable: false, name: 'feature_param', randomName: 'choice-parameter-11904002738577049', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["Get feature script error"]'], script: [classpath: [], sandbox: false, script: 'return [\'test1.feature\',\'test2.feature\']']]], [$class: 'CascadeChoiceParameter', choiceType: 'PT_MULTI_SELECT', description: '', filterLength: 1, filterable: false, name: 'tags_param', randomName: 'choice-parameter-11904002743983235', referencedParameters: 'feature_param', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["Get tags script error"]'], script: [classpath: [], sandbox: false, script: 'return [\'@tag1\',\'@tag2\']']]]]    }
+        input id: 'User_input_id', message: 'Please fill inputs', ok: 'Çalıştır', parameters: [[$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: '', filterLength: 1, filterable: false, name: 'feature_param', randomName: 'choice-parameter-11905049518345177', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["Get feature script error"]'], script: [classpath: [], sandbox: false, script: 'return [\'test1.feature\',\'test2.feature\']']]], [$class: 'CascadeChoiceParameter', choiceType: 'PT_MULTI_SELECT', description: '', filterLength: 1, filterable: false, name: 'tags_param', randomName: 'choice-parameter-11905049528045193', referencedParameters: 'feature_param', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["Get tags script error"]'], script: [classpath: [], sandbox: false, script: '''def folder = "/var/lib/jenkins/workspace/jenkins-pipeline-example/src/test/java/myTests"
+
+        def file = new File("${folder}/${feature_param}")
+        def lines = file as String[]
+        def tags = lines.findAll { it.trim().startsWith(\'@\') }.collect { 
+            it.replaceAll("\\\\s","")
+        }
+        
+        return tags''']]]]
+    }
 
     stage('Run karate tests') {
         def tags1 = ["scenario1", "@test1_scenario2", "test1_scenario3", "test1_scenario4"].join(",")
